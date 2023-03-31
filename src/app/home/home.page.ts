800989f5ae703a3afd0c80f4a2b0f7fafec08733
unknown <G00346045@atu.ie>
2023-03-31 11:30:52 +0000

    lab8

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import{MovieService} from '../Services/movie.service'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
-  constructor() {}
+myMovies:any[]=[];
+  constructor(private service:MovieService) {
+
+
+    
+  }
   
+  ionViewWillEnter(){
+    this.service.GetMovieData().subscribe(
+      (data)=>{
+        this.myMovies = data.Search;
+
+      }
+    );
+  }
+
 }
